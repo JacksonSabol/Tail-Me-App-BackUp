2437520fdd373fd025855a957d4eef18d7d5bd2d
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-31 10:04:56 -0800

    back up with new structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TailMe
+# Tail-Me-App
+
+## Remember to change credentials in config.json when necessary
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "../../index.css";
 const Footer = (props) => (
     <footer>
         <div>
-            Footer
+            Footer This
         </div>
     </footer>
 )
